2d3c67f9079fa5517e7d8072be67a7fe65192235
Kaio <kaiohtf38@gmail.com>
2026-01-23 10:50:12 -0300

    make `database.js` more robust

--- infra/database.js
+++ infra/database.js
@@ -7,9 +7,8 @@ async function query(queryObject) {
         user: process.env.POSTGRES_USER,
         database: process.env.POSTGRES_DB,
         password: process.env.POSTGRES_PASSWORD,
-        ssl: process.env.NODE_ENV === "development" ? false : true,
+        ssl: getSSLValue(),
     });
-    console.log(client.ssl)
     try{
         await client.connect();
         const result = await client.query(queryObject);
@@ -24,3 +23,13 @@ async function query(queryObject) {
 export default {
     query: query,
 };
+
+function getSSLValue(){
+    if (process.env.POSTGRES_CA){
+        return {
+            ca: process.env.POSTGRES_CA,
+
+        }
+    }
+    return process.env.NODE_ENV === "development" ? false : true
+}
